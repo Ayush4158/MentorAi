e32180f0db6b91f0c7cb26530ff2c0b086c2c6b2
Ayush4158 <ayushsingh8692@gmail.com>
2025-04-09 10:06:35 +0530

    - Added several new dependencies including Clerk for authentication and Radix UI components for better UI elements. - Updated package.json to reflect the new dependencies and removed unused files.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="px-5">
+    </div>
   );
 }
